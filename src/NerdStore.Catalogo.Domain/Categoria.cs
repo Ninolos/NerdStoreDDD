a3f5cd9f057c9b691b3b448add866483c6fe2e33
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-11-28 18:00:38 -0300

    #dev - aplicar validação dentro do construtor de Categoria

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -16,11 +16,19 @@ namespace NerdStore.Catalogo.Domain
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
-            Codigo = codigo;            
+            Codigo = codigo;
+
+            Validar();
         }
         public override string ToString()
         {
             return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
         }
     }
 }
