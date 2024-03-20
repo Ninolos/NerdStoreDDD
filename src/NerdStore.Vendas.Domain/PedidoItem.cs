917a30c372c2c2be3fe387c3e036ed91cd5349c2
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-03-20 09:23:35 -0300

    #dev - Setup inicial Vendas

---
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,13 @@
+﻿using NerdStore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem : Entity
+    {
+    }
+}
