fb00bc14959713ac5f61be8820005db7a9cff1c3
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-03-20 11:20:18 -0300

    #dev - propriedades Pedido

--- src/NerdStore.Vendas.Domain/Pedido.cs
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -10,5 +10,22 @@ namespace NerdStore.Vendas.Domain
 {
     public class Pedido : Entity, IAggregateRoot
     {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        //Lista interna para evitar manipulação
+        private readonly List<PedidoItem> _pedidoItems;
+        //Lista externa  
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        //EF Rel.
+        public Voucher Voucher { get; private set; }
+
     }
 }
