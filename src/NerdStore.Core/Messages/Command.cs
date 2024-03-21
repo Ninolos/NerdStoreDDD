dc4afd5e9b1991dc1441edaea8fece4d87aa8a04
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-03-21 18:19:01 -0300

    #dev - command para AdicionarItemAoPedido e suas validações com FluentValidation

--- src/NerdStore.Core/Messages/Command.cs
+++ src/NerdStore.Core/Messages/Command.cs
@@ -12,7 +12,7 @@ namespace NerdStore.Core.Messages
     public abstract class Command : Message, IRequest<bool>
     {
         public DateTime Timestamp { get; private set; }
-        public ValidationResult ValidationResult { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
 
         public Command()
         {
