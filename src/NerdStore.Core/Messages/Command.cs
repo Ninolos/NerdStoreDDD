3e7c25211c9b1d86e6f0df72631839bff16422f5
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-03-21 18:03:50 -0300

    #dev - classe Command

---
+++ src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command : Message, IRequest<bool>
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; private set; }
+
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
