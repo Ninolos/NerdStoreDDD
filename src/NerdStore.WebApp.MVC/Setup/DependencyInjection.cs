97b5a968dd198536615116cfa7059a62a5c7c206
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2024-03-21 18:27:53 -0300

    #dev - criando PedidoCommandHandler a ajustando dependencia

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -5,6 +5,7 @@ using NerdStore.Catalogo.Data;
 using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
 
 namespace NerdStore.WebApp.MVC.Setup
 {
@@ -22,6 +23,10 @@ namespace NerdStore.WebApp.MVC.Setup
             services.AddScoped<CatalogoContext>();
 
             services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+
+            //Vendas
+
+            services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
         }
     }
 }
